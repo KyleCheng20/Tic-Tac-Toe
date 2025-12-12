ce050eaa0d44b2edea3bd5aa64acc12140f28068
Kyle Cheng <kcheng120@gmail.com>
2025-12-12 16:59:03 -0500

    Create gameBoard module

---
+++ script.js
@@ -0,0 +1,30 @@
+const gameBoard = (function (){
+    const rows = 3;
+    const cols = 3;
+    const board = [];
+
+    function initBoard(){
+        for(let i = 0; i < rows; i++){
+            board[i] = [];
+            for(let j = 0; j < cols; j++){
+                board[i][j] = '';
+            }
+        }
+    }
+    initBoard();
+        
+    function getBoard(){
+        return board;
+    }
+
+    function resetBoard(){
+        initBoard();
+    }
+
+    function setCell(row, col, marker){
+        board[row][col] = marker;
+    }
+
+    return {getBoard, resetBoard, setCell};
+})();
+
